8fd0505f06a28de5c0ca1d226b5d4fb1a5b298fd
SheenaOreilly <oreils21@tcd.ie>
2025-01-23 22:31:30 +0000

    initial commit

---
+++ src/main/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_tour_carbon_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTourCarbonCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicTourCarbonCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.music_tour_carbon_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTourCarbonCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
