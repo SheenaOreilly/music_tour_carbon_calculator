f1d22ff9d7cc98d745c78c9ca1f8a27beb15350e
SheenaOreilly <oreils21@tcd.ie>
2025-02-04 23:23:58 +0000

    Completed Carbon Calculator for all forms of transport.

--- src/main/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplication.java
+++ src/main/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplication.java
@@ -23,6 +23,7 @@ public class MusicTourCarbonCalculatorApplication {
     public String thisModel;
     public String thisFuel;
     public String thisConsumption;
+    public String currentVehicle;
 
     public static void main(String[] args) {
         SpringApplication.run(MusicTourCarbonCalculatorApplication.class, args);
@@ -65,12 +66,47 @@ public class MusicTourCarbonCalculatorApplication {
         model.addAttribute("consumption", consumption);
         return "carbon";
     }
+    @GetMapping("/getPlanes")
+    @ResponseBody
+    public Map<String, String> getPlanes()  {
+        return PlaneInfo.getAirports();
+    }
 
-    @GetMapping("/getBusInfo")
-    public String getBusInfo(@RequestParam("bus") String bus){
+    @GetMapping("/getPlaneCarbon")
+    public String getPlaneCarbon(
+            @RequestParam(value = "dep", defaultValue = "dublin") String dep,
+            @RequestParam(value = "arr", defaultValue = "donegal") String arr,
+            Model model) {
+        String[] arrOfStr = dep.split(":");
+        double lat1 = Double.parseDouble(arrOfStr[0]);
+        double lon1 = Double.parseDouble(arrOfStr[0]);
+        arrOfStr = arr.split(":");
+        double lat2 = Double.parseDouble(arrOfStr[0]);
+        double lon2 = Double.parseDouble(arrOfStr[0]);
+        double distance = Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))*6371;
+        double carbon = getPlaneInfo(distance);
+        String carbonEmissions = String.format("%.2f", carbon);
+
+        model.addAttribute("distance", String.format("%.2f", distance));
+        model.addAttribute("vehicleFuel", "N/A");
+        model.addAttribute("consumption", "N/A");
+        model.addAttribute("carbonEmissions", carbonEmissions);
+
+        return "carbon";
+    }
+
+    public void getBusInfo(@RequestParam("bus") String bus){
         thisConsumption = BusInfo.getBusEmissions(bus);
         thisFuel = "diesel";
-        return "carbon";
+    }
+    public double getPlaneInfo(double distance){
+        if(distance < 1600){
+            return 0.4 * distance;
+        } else if(distance > 4000){
+            return 0.27 * distance;
+        } else{
+            return 0.25 * distance;
+        }
     }
 
     @GetMapping("/carbon")
@@ -83,9 +119,25 @@ public class MusicTourCarbonCalculatorApplication {
             @RequestParam(value = "origin", defaultValue = "dublin") String origin,
             @RequestParam(value = "destination", defaultValue = "dublin") String destination,
             @RequestParam(value = "mode", defaultValue = "driving") String mode,
+            @RequestParam(value = "vehicle", defaultValue = "train") String vehicle,
+            @RequestParam(value = "bus", defaultValue = "coach") String bus,
             Model model) throws IOException {
 
         double distance = Distance.calculateDistance(origin, destination, mode);
+
+        if(vehicle.equalsIgnoreCase("train")){
+            double carbon = 0.28 * distance;
+            String carbonEmissions = String.format("%.2f", carbon);
+            model.addAttribute("distance", distance);
+            model.addAttribute("vehicleFuel", "N/A");
+            model.addAttribute("consumption", "N/A");
+            model.addAttribute("carbonEmissions", carbonEmissions);
+            return "carbon";
+        }
+
+        if(vehicle.equalsIgnoreCase("bus")){
+            getBusInfo(bus);
+        }
 
         String carbonEmissions = Calculator.calculateCarbonEmissions(distance, thisFuel, Double.parseDouble(thisConsumption));
 
---
+++ src/main/java/com/example/music_tour_carbon_calculator/PlaneInfo.java
@@ -0,0 +1,42 @@
+package com.example.music_tour_carbon_calculator;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Service
+public class PlaneInfo {
+
+    public static Map<String, String> getAirports(){
+        Map<String, String> airports = new LinkedHashMap<>();
+        BufferedReader reader;
+        try{
+            reader = new BufferedReader(new FileReader("C:\\Users\\Admin\\Documents\\FifthYear\\dissertation\\project\\music_tour_carbon_calculator\\src\\main\\resources\\static\\GlobalAirportDatabase.txt"));
+            String line = reader.readLine();
+
+            while(line != null){
+                String str = line;
+                String[] arrOfStr = str.split(":");
+                String airport = arrOfStr[2];
+                if(airport.equalsIgnoreCase("N/A")){
+                    airport = arrOfStr[3];
+                }
+                String lat = arrOfStr[14];
+                String lon = arrOfStr[15];
+                String combined = lat + ":" + lon;
+                airports.put(airport, combined);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return airports;
+    }
+
+
+}
