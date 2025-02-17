1511f064446694ea291a80157c4ba2abe81dbb31
SheenaOreilly <oreils21@tcd.ie>
2025-02-17 23:22:32 +0000

    Fixed Object classes

--- src/main/java/com/example/music_tour_carbon_calculator/TourData.java
+++ src/main/java/com/example/music_tour_carbon_calculator/TourData.java
@@ -1,45 +1,52 @@
 package com.example.music_tour_carbon_calculator;
 
-public class oneEntryObject {
-    String arrival;
-    double carbon_emissions;
-    String departure;
-    double distance;
-    String fuel;
-    String consumption;
+public class TourData {
+    private String departure;
+    private String arrival;
+    private String distance;
+    private String consumption;
+    private String fuel;
+    private String carbonEmissions;
 
-    public oneEntryObject() {}
+    public TourData() {}
 
-    public oneEntryObject(String arrival, double carbon_emissions, String departure, double distance, String fuel, String consumption){
-        this.arrival = arrival;
-        this.carbon_emissions = carbon_emissions;
+    public TourData(String departure, String arrival, String distance, String consumption, String fuel, String carbonEmissions) {
         this.departure = departure;
+        this.arrival = arrival;
         this.distance = distance;
-        this.fuel = fuel;
         this.consumption = consumption;
+        this.fuel = fuel;
+        this.carbonEmissions = carbonEmissions;
     }
 
-    public String getArrival() {
-        return arrival;
-    }
+    // Getters and Setters
+    public String getDeparture() { return departure; }
+    public void setDeparture(String departure) { this.departure = departure; }
 
-    public double getCarbon_emissions() {
-        return carbon_emissions;
-    }
+    public String getArrival() { return arrival; }
+    public void setArrival(String arrival) { this.arrival = arrival; }
 
-    public String getDeparture() {
-        return departure;
-    }
+    public String getDistance() { return distance; }
+    public void setDistance(String distance) { this.distance = distance; }
 
-    public double getDistance() {
-        return distance;
-    }
+    public String getConsumption() { return consumption; }
+    public void setConsumption(String consumption) { this.consumption = consumption; }
 
-    public String getFuel() {
-        return fuel;
-    }
+    public String getFuel() { return fuel; }
+    public void setFuel(String fuel) { this.fuel = fuel; }
 
-    public String getConsumption() {
-        return consumption;
+    public String getCarbonEmissions() { return carbonEmissions; }
+    public void setCarbonEmissions(String carbonEmissions) { this.carbonEmissions = carbonEmissions; }
+
+    @Override
+    public String toString() {
+        return "TourData{" +
+                "departure='" + departure + '\'' +
+                ", arrival='" + arrival + '\'' +
+                ", distance='" + distance + '\'' +
+                ", consumption='" + consumption + '\'' +
+                ", fuel='" + fuel + '\'' +
+                ", carbonEmissions='" + carbonEmissions + '\'' +
+                '}';
     }
 }
--- src/main/java/com/example/music_tour_carbon_calculator/tourObject.java
+++ src/main/java/com/example/music_tour_carbon_calculator/tourObject.java
@@ -1,2 +1,21 @@
-package com.example.music_tour_carbon_calculator;public class tourObject {
+package com.example.music_tour_carbon_calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class tourObject {
+    List<TourData> legsOfTour = new ArrayList<>();
+    public String tourName;
+
+    public tourObject(String tourName){
+        this.tourName = tourName;
+    }
+
+    public void add(TourData oneLeg){
+        legsOfTour.add(oneLeg);
+    }
+
+
+
 }
