30eac7ca276b10afbee39b6037c677f9c27bc2b0
SheenaOreilly <oreils21@tcd.ie>
2025-03-22 21:26:28 +0000

    Added selenium tests for new tour page.

--- src/test/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplicationTests.java
+++ src/test/java/com/example/music_tour_carbon_calculator/MusicTourCarbonCalculatorApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.music_tour_carbon_calculator;
 import com.example.music_tour_carbon_calculator.controllers.MusicTourCarbonCalculatorApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(classes = MusicTourCarbonCalculatorApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MusicTourCarbonCalculatorApplication.class)
 class MusicTourCarbonCalculatorApplicationTests {
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void contextLoads() {
+        // The test will pass if the context loads successfully
     }
 }
